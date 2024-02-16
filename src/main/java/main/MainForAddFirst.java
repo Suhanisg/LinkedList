b1e2e93682a97a737d1dbdd221245653cdf1f0f2
Suhanisg <suhugoyal@gmail.com>
2024-02-16 23:15:28 +0530

    MainForAddFirst

---
+++ src/main/java/main/MainForAddFirst.java
@@ -0,0 +1,11 @@
+package main;
+
+public class MainForAddFirst {
+    public static void main(String[] args){
+        addfirst af = new addfirst();
+        af.addFirst(1);
+        af.addFirst(3);
+        af.addFirst(4);
+        af.Print();
+    }
+}
