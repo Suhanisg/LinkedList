ad806d3237f619c84dba44918d1b60edaa6fea6c
Suhanisg <suhugoyal@gmail.com>
2024-02-16 23:17:55 +0530

    MainForAddLast

---
+++ src/main/java/main/MainForAddLast.java
@@ -0,0 +1,11 @@
+package main;
+
+public class MainForAddLast {
+    public static void main(String[] args){
+        addlast al = new addlast();
+        al.addLast(7);
+        al.addLast(3);
+        al.Print();
+    }
+}
+
