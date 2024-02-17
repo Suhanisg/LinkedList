5b098b4a917d32bb1d260a43131981000ad8af92
Suhanisg <suhugoyal@gmail.com>
2024-02-17 21:31:46 +0530

    LinkedList1

--- src/main/java/main/LinkedList1.java
+++ src/main/java/main/LinkedList1.java
@@ -1,8 +1,62 @@
 package main;
 
 public class LinkedList1 {
-    public static void main(String[] args) {
-        System.out.println("Helloo");
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head= tail= newNode ;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
 
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public void print(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+ "  ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public void add(int idx,int data){
+        Node newNode=new Node(data);
+        Node temp=head;
+        int i=0;
+        while(i< idx-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+
+    public static void main(String[] args) {
+        LinkedList1 ll=new LinkedList1();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2,6);
+        ll.print();
     }
 }
